4dbc23aaca351c12541f6c7093db7dd5002cb95d
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-08-11 22:12:09 -0500

    Crear validacion de datos del formulario

--- lib/controllers/profil_controller.dart
+++ lib/controllers/profil_controller.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:piter_getx/models/user.dart';
 
 class ProfilController extends GetxController {
   User _usuario;
+
+  int _campoEdad = 0;
 
   User get obtenerUsuario => _usuario;
 
@@ -10,5 +13,30 @@ class ProfilController extends GetxController {
   void onInit() {
     super.onInit();
     this._usuario = Get.arguments as User;
+  }
+
+  void capturarCambiosDelTexto(String edad) {
+    this._campoEdad = int.parse(edad);
+  }
+
+  void validarEdad() {
+    if (this._campoEdad >= 18) {
+      Get.back(result: this._campoEdad);
+    } else {
+      Get.dialog(
+        AlertDialog(
+          title: Text('Error edad no permitida'),
+          content: Text('Ingrese un valor válido'),
+          actions: [
+            FlatButton(
+              onPressed: () {
+                Get.back();
+              },
+              child: Text('Aceptar'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 }

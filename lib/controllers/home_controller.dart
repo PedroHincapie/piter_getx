53b4e50649ddcd937892597c1c4bcfda37a8baef
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-08-11 19:16:00 -0500

    Update home_controller.dart

--- lib/controllers/home_controller.dart
+++ lib/controllers/home_controller.dart
@@ -1,7 +1,13 @@
 import 'package:get/get.dart';
+import 'package:piter_getx/api/users_api.dart';
+import 'package:piter_getx/models/user.dart';
 
 class HomeController extends GetxController {
   int _contador = 0;
+
+  List<User> _listaUsers = [];
+
+  List<User> get listaUsers => _listaUsers;
 
   int get contador => _contador;
 
@@ -13,6 +19,12 @@ class HomeController extends GetxController {
   @override
   void onReady() {
     super.onReady();
+  }
+
+  Future<void> cargarUsarios() async {
+    final listaUsuarios = await UsersAPI.intancia.obtenerUsuarios(1);
+    this._listaUsers = listaUsuarios;
+    update(['users']);
   }
 
   void incrementarContador() {

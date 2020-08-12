22ba395b2fc69befa62ba490c6082c16b0b0b8eb
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-08-11 21:17:17 -0500

    Update profile_page.dart

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
 import 'package:piter_getx/controllers/profil_controller.dart';
 
@@ -8,9 +9,39 @@ class ProfilePage extends StatelessWidget {
     return GetBuilder<ProfilController>(
       init: ProfilController(),
       builder: (_) => Scaffold(
-        body: Center(
-          child: Text(
-            '${_.obtenerUsuario.firstName}',
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back), onPressed: () => Get.back()),
+        ),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                '${_.obtenerUsuario.firstName}',
+              ),
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text('Aceptar'),
+                elevation: 10.6,
+                animationDuration: Duration(milliseconds: 600),
+                onPressed: () {},
+                color: Colors.green,
+              )
+            ],
           ),
         ),
       ),

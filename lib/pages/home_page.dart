8f826f588e09fae1cda038a822679929f1981f5b
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-08-17 15:49:02 -0500

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:piter_getx/controllers/global_controller.dart';
 import 'package:piter_getx/controllers/home_controller.dart';
-import 'package:piter_getx/pages/home_page_widgets/home_list.dart';
+import 'package:piter_getx/widgets/product_list.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -15,10 +16,25 @@ class HomePage extends StatelessWidget {
   Scaffold buildScaffold(HomeController _) {
     print('build home');
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          GetBuilder<GlobalControllers>(
+            id: 'favoritos',
+            builder: (_) => FlatButton(
+              onPressed: () {},
+              child: Text(
+                'Favoritos ${_.favoritos.length}',
+                style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+      ),
 //      body: Center(
 //        child: HomeLabel(),
 //      ),
-      body: HomeList(),
+//      body: HomeList(),
+      body: ProductList(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _.incrementarContador();

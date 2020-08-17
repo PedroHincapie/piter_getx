0174d971ab9650c4d1d2840c15722df995f69a45
Pedro Jesus Hincapie Garcia <pedandniv@gmail.com>
2020-08-17 15:43:16 -0500

    Adicionar el estado global en la APP

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:piter_getx/controllers/global_controller.dart';
 import 'package:piter_getx/pages/splash_page.dart';
 
 void main() => runApp(MyApp());
@@ -7,6 +8,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Get.put(GlobalControllers());
     return GetMaterialApp(
       title: 'Getx App',
       debugShowCheckedModeBanner: false,
